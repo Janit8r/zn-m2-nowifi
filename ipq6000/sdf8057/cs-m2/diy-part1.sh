80da4e71b926f1d8c673e8f2e4bae1409eef48e5
VIPGPT6 <136162218+VIPGPT6@users.noreply.github.com>
2023-07-01 09:52:00 +0800

    Update diy-part1.sh

--- ipq6000/sdf8057/cs-m2/diy-part1.sh
+++ ipq6000/sdf8057/cs-m2/diy-part1.sh
@@ -11,10 +11,10 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*liuran001_packages\)/\1/' feeds.conf.default
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
-sed -i '$a src-git clone https://github.com/liuran001/openwrt-packages' feeds.conf.default
+src-git liuran001_packages https://github.com/liuran001/openwrt-packages
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
