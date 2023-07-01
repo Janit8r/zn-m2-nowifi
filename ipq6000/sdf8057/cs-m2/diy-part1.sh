6ad806a499f1f0fb17abd25a9973c21e7a12dfd1
VIPGPT6 <136162218+VIPGPT6@users.noreply.github.com>
2023-07-01 09:19:54 +0800

    Update diy-part1.sh

--- ipq6000/sdf8057/cs-m2/diy-part1.sh
+++ ipq6000/sdf8057/cs-m2/diy-part1.sh
@@ -5,14 +5,16 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
-# https://githubfast.com/P3TERX/Actions-OpenWrt
+# https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://githubfast.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git kiddin9 https://githubfast.com/kiddin9/openwrt-packages' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
+sed -i '$a src-git clone https://github.com/liuran001/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
